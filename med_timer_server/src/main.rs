606a6b2f0ce21d2e2a3744f5406825fd10ffbd28
Aaron <>
2021-09-18 19:20:28 +0100

    improved error handling and main method

--- med_timer_server/src/main.rs
+++ med_timer_server/src/main.rs
@@ -1,32 +1,55 @@
 #![feature(async_closure)]
 
 mod routes;
-use log::LevelFilter;
-use routes::{entries, meds};
-
 use actix_web::{middleware, App, HttpServer};
+use anyhow::anyhow;
+use log::LevelFilter;
 use paperclip::actix::OpenApiExt;
+use routes::{entries, meds};
 use sqlx::SqlitePool;
-use std::io::Write;
+use std::{io::Write, path::Path};
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    // load env vars from file, discard result
-    // this is as it's totally okay for it to not load a dotenv file, as env vars can be passed in in other ways
-    let _ = dotenv::dotenv();
+struct EnvVars {
+    pub database_url: String,
+    pub logging_level: LevelFilter,
+}
 
-    let database_url = std::env::var("DATABASE_URL").expect("no database url");
-    let logging_level = std::env::var("LOGGING_LEVEL")
-        .unwrap_or_else(|_| "info".to_string())
-        .parse()
-        .expect("invalid logging level");
+impl EnvVars {
+    /// Reads env vars from a file if it exists, and then fetch all the env vars needed.
+    pub fn new() -> anyhow::Result<Self> {
+        // load env vars from file, discard result
+        // this is as it's totally okay for it to not load a dotenv file, as env vars can be passed in in other ways
+        let _ = dotenv::dotenv();
 
-    // simple workaround to stop sqlx::query info-level logs showing up at info level, since they should be at debug
-    let sqlx_query_logging_level = match logging_level {
-        LevelFilter::Info => LevelFilter::Warn,
-        _ => logging_level,
-    };
+        // then try and read all needed env vars
+        let database_url = match std::env::var("DATABASE_URL") {
+            Ok(path) if Path::new(&path).exists() => path,
+            Ok(_) => return Err(anyhow!("DATABASE_URL points to a non-existent file")),
+            Err(_) => return Err(anyhow!("DATABASE_URL not set")),
+        };
 
+        // if LOGGING_LEVEL is not set, just default to info
+        let logging_level = std::env::var("LOGGING_LEVEL")
+            .unwrap_or_else(|_| "info".to_string())
+            .parse()
+            .map_err(|_| anyhow!(r#"LOGGING_LEVEL could not be parsed: try setting to one of ["off", "error", "warn", "info", "debug", "trace"]"#))?;
+
+        Ok(Self {
+            database_url,
+            logging_level,
+        })
+    }
+
+    /// Returns the correct logging level for the `sqlx::query` module.
+    pub fn sqlx_query_logging_level(&self) -> LevelFilter {
+        match self.logging_level {
+            LevelFilter::Info => LevelFilter::Warn,
+            _ => self.logging_level,
+        }
+    }
+}
+
+fn initialise_logger(env: &EnvVars) {
     // create logger with format `TIMESTAMP [LEVEL] LOG`
     let mut builder = env_logger::builder();
     builder.format(|buf, record| {
@@ -38,19 +61,25 @@ async fn main() -> std::io::Result<()> {
             record.args()
         )
     });
-    builder.filter_level(logging_level);
-    builder.filter_module("sqlx::query", sqlx_query_logging_level);
-    builder.init();
 
-    // then create database pool, mapping to io error if needed
-    let pool = SqlitePool::connect(&database_url)
-        .await
-        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+    // set filter levels - sqlx::query logs at a weird level so needs a special rule to make logs tidier
+    builder.filter_level(env.logging_level);
+    builder.filter_module("sqlx::query", env.sqlx_query_logging_level());
+    builder.init();
+}
+
+#[actix_web::main]
+async fn main() -> anyhow::Result<()> {
+    let env = EnvVars::new()?;
+    initialise_logger(&env);
+
+    let pool = SqlitePool::connect(&env.database_url).await?;
 
     let server = HttpServer::new(move || {
         App::new()
             .wrap(middleware::NormalizePath::default())
             .wrap(middleware::Logger::default())
+            .wrap(middleware::Compress::default())
             .data(pool.clone())
             .wrap_api()
             .with_json_spec_at("/spec/")
@@ -60,5 +89,7 @@ async fn main() -> std::io::Result<()> {
     });
 
     // finally bind the server to an address and run
-    server.bind("127.0.0.1:8080")?.run().await
+    server.bind("127.0.0.1:8080")?.run().await?;
+
+    Ok(())
 }
--- med_timer_server/src/routes/mod.rs
+++ med_timer_server/src/routes/mod.rs
@@ -1,6 +1,7 @@
 use paperclip::actix::Apiv2Schema;
 use paste::paste;
 use serde::Deserialize;
+use sqlx::Error;
 
 pub(crate) mod entries;
 pub(crate) mod meds;
@@ -44,6 +45,79 @@ generate_default_query! {
     }
 }
 
+/// Simple function to log the correct response if a database query fails.
+#[inline(always)]
+fn log_error(err: Error) {
+    match err {
+        Error::Database(err) => {
+            log::error!("Error returned from database: {}", err);
+        }
+        Error::Io(err) => {
+            log::error!("IO error while communicating with database: {}", err);
+        }
+        Error::Tls(err) => {
+            log::error!(
+                "Unable to establish TLS connection while communicating with database: {}",
+                err
+            );
+        }
+        Error::Protocol(err) => {
+            log::error!("Protocl error while communicating with database: {}", err);
+        }
+        Error::RowNotFound => {
+            log::trace!("No rows returned from database query");
+        }
+        Error::TypeNotFound { type_name } => {
+            log::error!("Unable to find type for database query: {}", type_name);
+        }
+        Error::ColumnIndexOutOfBounds { index, len } => {
+            log::error!(
+                "Column index out of bounds while performing database query: index {}; len {}",
+                index,
+                len
+            );
+        }
+        Error::ColumnNotFound(column) => {
+            log::error!("Column not found in database: {}", column);
+        }
+        Error::ColumnDecode { index, source } => {
+            log::error!(
+                "Unable to decode column in database: {}; source {}",
+                index,
+                source
+            );
+        }
+        Error::Decode(err) => {
+            log::error!("Unable to decode value from database: {}", err);
+        }
+        Error::PoolTimedOut => {
+            log::warn!("No database pool connection available");
+        }
+        Error::PoolClosed => {
+            log::warn!("Database pool closed while trying to acquire connection");
+        }
+        Error::WorkerCrashed => {
+            log::error!("Background worker for database pool crashed");
+        }
+        err => {
+            log::error!("Unknown error while performing database query: {}", err);
+        }
+    };
+}
+
+fn handle_entry<T>(entry: Result<T, Error>) -> Option<T> {
+    match entry {
+        Ok(entry) => Some(entry),
+        Err(Error::Configuration(err)) => {
+            panic!("FATAL: Issue with DB configuration: {}", err)
+        }
+        Err(err) => {
+            log_error(err);
+            None
+        }
+    }
+}
+
 /// Generates a database query using the given pool, query information (such as results limit), out type and the query itself.
 #[macro_export]
 macro_rules! query {
@@ -56,7 +130,7 @@ macro_rules! query {
         Json(sqlx::query_as::<_, $out_struct>($query)
             $(.bind($args))*
             .fetch($pool.as_ref())
-            .filter_map(async move |entry| entry.ok())
+            .filter_map(async move |entry| crate::routes::handle_entry(entry))
             .take($url_queries.count_or_default())
             .collect()
             .await)
